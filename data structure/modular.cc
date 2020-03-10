3eacc176462c61399e8e4b2e430b70564bf8ae44
ruzs <ruz@outlook.kr>
2020-03-10 20:41:59 +0900

    .

---
+++ data structure/modular.cc
@@ -0,0 +1,59 @@
+template <const int mod>
+class modular {
+	int x;
+public:
+	template <typename U>
+	modular(const U& v = 0) : x(v) {
+		x %= mod;
+		if (x < 0) x += mod;
+	}
+	template <typename U>
+	modular pow(U e) {
+		modular a = x, r = 1;
+		for (; e > 0; a *= a, e >>= 1) if (e & 1) r *= a;
+		return r;
+	}
+	modular inv() { return pow(mod - 2); }
+
+	template <typename U> auto& operator =(const U& b) { x = b; return *this; }
+	auto& operator=(const modular& rhs) { x = rhs.x; return *this; }
+	template <typename U> auto& operator+=(const U& b) { if ((x += b) >= mod) x -= mod; return *this; }
+	template <typename U> auto& operator-=(const U& b) { if ((x -= b) < 0) x += mod; return *this; }
+	template <typename U> auto& operator*=(const U& b) { x = b * x - static_cast<long long>(static_cast<long double>(b) * x / mod) * mod; return *this; }
+	template <typename U> auto& operator/=(const U& b) { return *this *= modular(b).inv(); }
+	auto& operator+=(const modular& rhs) { return *this += rhs.x; }
+	auto& operator-=(const modular& rhs) { return *this -= rhs.x; }
+	auto& operator*=(const modular& rhs) { return *this *= rhs.x; }
+	auto& operator/=(const modular& rhs) { return *this /= rhs.x; }
+	auto& operator++() { x = x + 1 == mod ? 0 : x + 1; return *this; }
+	auto& operator--() { x = !x ? mod - 1 : x - 1; return *this; }
+	auto operator++(int) { int r = x; ++x; return r; }
+	auto operator--(int) { int r = x; --x; return r; }
+	auto operator-(int) { return !x ? 0 : mod - x; }
+	auto operator==(const modular& rhs) { return x == rhs.x; }
+	auto operator!=(const modular& rhs) { return x != rhs.x; }
+
+	friend string to_string(modular& m) { return to_string(m.x); }
+	friend auto operator+(const modular& lhs, const modular& rhs) { return modular(lhs) += rhs; }
+	friend auto operator-(const modular& lhs, const modular& rhs) { return modular(lhs) -= rhs; }
+	friend auto operator*(const modular& lhs, const modular& rhs) { return modular(lhs) *= rhs; }
+	friend auto operator/(const modular& lhs, const modular& rhs) { return modular(lhs) /= rhs; }
+	friend auto operator==(const modular& lhs, const modular& rhs) { return lhs.x == rhs.x; }
+	friend auto operator!=(const modular& lhs, const modular& rhs) { return lhs.x != rhs.x; }
+	template <typename U> friend auto operator+(const modular& lhs, const U& b) { return modular(lhs) += b; }
+	template <typename U> friend auto operator-(const modular& lhs, const U& b) { return modular(lhs) -= b; }
+	template <typename U> friend auto operator*(const modular& lhs, const U& b) { return modular(lhs) *= b; }
+	template <typename U> friend auto operator/(const modular& lhs, const U& b) { return modular(lhs) /= b; }
+	template <typename U> friend auto operator==(const modular& lhs, const U& b) { return lhs.x == b; }
+	template <typename U> friend auto operator!=(const modular& lhs, const U& b) { return lhs.x != b; }
+	template <typename U> friend auto operator+(const U& b, const modular& rhs) { return modular(b) += rhs; }
+	template <typename U> friend auto operator-(const U& b, const modular& rhs) { return modular(b) -= rhs; }
+	template <typename U> friend auto operator*(const U& b, const modular& rhs) { return modular(b) *= rhs; }
+	template <typename U> friend auto operator/(const U& b, const modular& rhs) { return modular(b) /= rhs; }
+	template <typename U> friend auto operator==(const U& b, const modular& rhs) { return b == rhs.x; }
+	template <typename U> friend auto operator!=(const U& b, const modular& rhs) { return b != rhs.x; }
+	friend auto& operator>>(istream& in, modular& m) { cin >> m.x; return in; 	}
+	friend auto& operator<<(ostream& out, modular& m) { cout << m.x; return out; }
+};
+
+using mint = modular<998244353>;
